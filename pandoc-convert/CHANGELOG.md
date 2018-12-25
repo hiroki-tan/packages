f8dcd32e483b2ecb8549daf3e70162dd9750bff7
丹 裕樹 <hiroki_tan@gruff.co.jp>
2018-12-25 11:56:17 +0900

    Atomパッケージ追加

---
+++ pandoc-convert/CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## 1.1.0
+* Configuration option to use custom `pandoc` binary
+* Fix cwd when executing `pandoc`
+
+## 1.0.0
+* Initial
---
+++ pandoc-convert/lib/init.js
@@ -0,0 +1,116 @@
+"use babel"
+
+import { existsSync } from 'fs'
+import { CompositeDisposable } from 'atom'
+import { execFile } from 'child_process'
+import { dirname } from 'path'
+import pandocBin from 'pandoc-bin'
+import targets from './targets'
+import SelectPathView from './views/select-path-view'
+
+const {
+  commands,
+  config,
+  notifications,
+  workspace
+} = atom
+
+const cache = {}
+
+module.exports = {
+
+  config: {
+    pandocBinary: {
+      description: 'Path to your `pandoc` binary. Default is the packaged version of pandoc.',
+      type: 'string',
+      default: ''
+    }
+  },
+
+  activate() {
+    if (atom.inDevMode() && !atom.inSpecMode()) {
+      console.log('activate pandoc-convert')
+    }
+
+    this.subscriptions = new CompositeDisposable()
+    this.selectPathView = new SelectPathView()
+
+    Object.keys(targets).forEach(target => {
+      const action = `pandoc-convert:${target.replace(/_/g, '-')}`
+
+      this.subscriptions.add(commands.add('atom-workspace', action, () => {
+        this.convertCommand(target)
+      }))
+    })
+  },
+
+  deactivate() {
+    this.subscriptions.dispose()
+  },
+
+  convertCommand(target) {
+
+    const editor = workspace.getActiveTextEditor()
+
+    if (!editor) {
+      return this.error(`Current item is not an editor.`)
+    }
+
+    if (editor.isModified() || !editor.getPath()) {
+      return this.error(`Text is modified. Please safe first.`)
+    }
+
+    const format = targets[target].format || target
+    const ext = targets[target].ext
+    const dpath = this.defaultOuputPath(ext)
+    const ipath = editor.getPath()
+
+    this.selectPathView.show(dpath, opath => {
+      cache[dpath] = opath
+      this.convert(format, ipath, opath)
+    })
+  },
+
+  error(message) {
+    notifications.addError(`[pandoc-convert]<br>${message}`)
+  },
+
+  success(message) {
+    notifications.addSuccess(`[pandoc-convert]<br>${message}`)
+  },
+
+  convert(format, ipath, opath) {
+
+    const pandocPath = config.get('pandoc-convert.pandocBinary') || pandocBin.path
+
+    if (!existsSync(pandocPath)) {
+      return this.error(`Binary \`${pandocPath}\` does not exist.`)
+    }
+
+    const cwd = dirname(ipath)
+
+    execFile(pandocPath, [
+      '--standalone',
+      `--to=${format}`,
+      `--output=${opath}`,
+      ipath
+    ], { cwd }, (error) => {
+      if (error) {
+        this.error(error.message)
+      } else {
+        this.success(`**Created:** \`${opath}\``)
+      }
+    })
+  },
+
+  defaultOuputPath(ext) {
+    const editor = workspace.getActiveTextEditor()
+    const dpath = `${editor.getPath()}.${ext}`
+
+    if (cache[dpath]) {
+      return cache[dpath]
+    }
+
+    return dpath
+  }
+}
